bfb9a7076e93c02a034997799c42515a5c89c656
Gianluca Azzollini <gianlucaazzollini@MBP-di-Gianluca.lan>
2020-08-04 17:43:32 +0200

    init

---
+++ project new/main.js
@@ -0,0 +1,14 @@
+var startExperienteBtn = document.getElementById('start_experience');
+
+startExperienteBtn.onclick = function(){
+    document.getElementById('container').outerHTML = '';
+    document.getElementsByTagName('a-scene')[0].style.zIndex = 'auto';
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    var scene = document.querySelector('a-scene');
+    var splash = document.querySelector('#splash');
+    scene.addEventListener('loaded', function (e) {
+        splash.style.display = 'none';
+    });
+});
---
+++ project new/tasto.js
@@ -0,0 +1,4 @@
+document.querySelector("button").onclick = function() {
+  document.querySelector("#loading").style.display = "none"
+}
+
